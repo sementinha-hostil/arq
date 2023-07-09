d5d4b84ab399051804a61071ea75f4a16a94f7ff
sementinha-hostil <136841545+sementinha-hostil@users.noreply.github.com>
2023-07-09 19:16:13 -0300

    Atualizar o mapp.py

--- mapp.py
+++ mapp.py
@@ -1,22 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import smtplib
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Bem-vindo ao servidor Flask!'
-
 @app.route('/get-ip', methods=['GET'])
 def get_ip():
-    ip = request.headers.get('X-Real-IP', request.remote_addr)
+    ip = request.remote_addr
 
     # Configuração do servidor de email
     email_server = 'smtp.mail.yahoo.com'
     email_port = 465
     email_sender = 'maria_crisostom0@yahoo.com'
     email_password = 'prosopopei4'
-    email_recipient = 'minadoooo112@yahoo.com'
+    email_recipient = 'minadoooo11@yahoo.com'
 
     # Criação da mensagem
     subject = 'Endereço IP do Usuário'
@@ -29,9 +25,9 @@ def get_ip():
             server.login(email_sender, email_password)
             server.sendmail(email_sender, email_recipient, email_text)
     except Exception as e:
-        return jsonify({'error': f'Erro ao enviar o email: {str(e)}'})
+        return f'Erro ao enviar o email: {str(e)}'
 
-    return jsonify({'ip': ip})
+    return 'Email enviado com sucesso!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
