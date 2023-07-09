f92caef79d909e76d5bc2b9ce0ccbd854c59bf82
sementinha-hostil <136841545+sementinha-hostil@users.noreply.github.com>
2023-07-09 19:35:44 -0300

    Atualizar o mapp.py

--- mapp.py
+++ mapp.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/get-ip', methods=['GET'])
 def get_ip():
-    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    ip = request.headers.get('X-Real-IP', request.remote_addr)
 
-    # Configuração do servidor de email
+    # Configuração do servidor de e-mail
     email_server = 'smtp.mail.yahoo.com'
     email_port = 465
     email_sender = 'maria_crisostom0@yahoo.com'
@@ -19,13 +19,13 @@ def get_ip():
     message = f'O endereço IP do usuário é: {ip}'
     email_text = f'Subject: {subject}\n\n{message}'
 
-    # Envio do email
+    # Envio do e-mail
     try:
         with smtplib.SMTP_SSL(email_server, email_port) as server:
             server.login(email_sender, email_password)
             server.sendmail(email_sender, email_recipient, email_text)
     except Exception as e:
-        return jsonify({'error': f'Erro ao enviar o email: {str(e)}'})
+        return jsonify({'error': f'Erro ao enviar o e-mail: {str(e)}'})
 
     return jsonify({'ip': ip})
 
