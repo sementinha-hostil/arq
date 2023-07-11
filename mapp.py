7e46a1885ce3e782d041bbf10f5fb0117d61e84f
sementinha-hostil <136841545+sementinha-hostil@users.noreply.github.com>
2023-07-10 22:09:11 -0300

    Atualizar o mapp.py

--- mapp.py
+++ mapp.py
@@ -1,35 +1,15 @@
-from sqlalchemy import create_engine, Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import requests
-import datetime
+from flask import Flask, request
+import os
 
-Base = declarative_base()
+app = Flask(__name__)
 
-class Usuario(Base):
-    __tablename__ = 'usuarios'
-    id = Column(String, primary_key=True)
-    ip = Column(String)
-    data_adicao = Column(DateTime)
-
-def salvar_ip_no_banco(ip):
-    engine = create_engine('sqlite:////storage/emulated/0/db/projetineo.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    usuario = Usuario(id='1', ip=ip, data_adicao=datetime.datetime.now())
-    session.add(usuario)
-    session.commit()
-    session.close()
-
-def obter_ip():
-    response = requests.get('https://api.ipify.org?format=json')
-    ip = response.json()['ip']
-    return ip
-
-def main():
-    ip = obter_ip()
-    salvar_ip_no_banco(ip)
-    print('IP salvo no banco de dados com sucesso.')
+@app.route('/salvar-ip', methods=['POST'])
+def salvar_ip():
+    ip = request.json['ip']
+    caminho_arquivo = '/storage/emulated/0/db/ips.txt'
+    with open(caminho_arquivo, 'a') as arquivo:
+        arquivo.write(ip + '\n')
+    return '', 200
 
 if __name__ == '__main__':
-    main()
+    app.run()
